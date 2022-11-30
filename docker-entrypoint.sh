b5a84d5c621384b6dbdefd0f4ccd922d6464f01c
exp.liao <289065406@qq.com>
2022-12-01 01:01:29 +0800

    更新脚本

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
 STEAM_DIR="/home/steam"
-STEAM_CMD_DIR="${STEAM_DIR}/steamcmd"
 STEAM_GAME_DIR="${STEAM_DIR}/games"
 STEAM_SAVE_DIR="${STEAM_DIR}/saved"
 ARK_APP_DIR="${STEAM_GAME_DIR}/ark"
 ARK_SAVE_DIR="${STEAM_SAVE_DIR}/ark"
 
-# 确保 steam 可读写挂载目录的权限  
-mkdir -p ${ARK_APP_DIR}
-mkdir -p ${ARK_SAVE_DIR}
-chown -R 1000:1000 ${STEAM_SAVE_DIR}
-
-# 如果还没下载 ARK 则设置权限？
-# chown -R 1000:1000 ${STEAM_GAME_DIR}
 
 
 # 保持前台运行，避免 docker 挂起
