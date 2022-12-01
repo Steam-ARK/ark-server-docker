11aa49bf5379b642224130c990cfacbbf2530a7f
EXP <289065406@qq.com>
2022-12-02 00:44:48 +0800

    新增备份支持

---
+++ backup/README.md
@@ -0,0 +1,5 @@
+# 存档/配置备份目录
+
+------
+
+若需要恢复存档，选择期望的备份，解压并覆盖到 `./volumes/steam/games/ark/ShooterGame/Saved/` 目录下即可。
--- bin/backup.ps1
+++ bin/backup.ps1
@@ -3,3 +3,21 @@
 #------------------------------------------------
 # 示例：bin/backup.ps1
 #------------------------------------------------
+
+$STEAM_DIR = "./volumes/steam"
+$STEAM_GAME_DIR = "${STEAM_DIR}/games"
+$ARK_APP_DIR = "${STEAM_GAME_DIR}/ark"
+$ARK_SAVED_DIR = "${ARK_APP_DIR}/ShooterGame/Saved/SavedArks"
+$ARK_CONFIG_DIR = "${ARK_APP_DIR}/ShooterGame/Saved/Config"
+
+$BACKUP_DIR = "./backup"
+$VERSION = Get-Date -format "yyMMddHH"
+$LATEST_BACKUP_DIR = "${BACKUP_DIR}/${VERSION}"
+
+if (Test-Path ${LATEST_BACKUP_DIR}) {
+    Remove-Item ${LATEST_BACKUP_DIR} -Recurse -Force
+}
+mkdir ${LATEST_BACKUP_DIR}
+
+Copy-Item "${ARK_SAVED_DIR}/*" -Destination ${LATEST_BACKUP_DIR}
+Copy-Item "${ARK_CONFIG_DIR}/*" -Destination ${LATEST_BACKUP_DIR}
