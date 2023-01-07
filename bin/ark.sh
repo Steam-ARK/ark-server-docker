67fd38d30da5cfb5fb9644b4707356dd251d1120
ubuntu <ubuntu@localhost.localdomain>
2023-01-07 17:38:58 +0800

    unlimit Structure

--- bin/ark.sh
+++ bin/ark.sh
@@ -91,6 +91,9 @@ nohup ${STEAM_ARK_DIR}/ShooterGame/Binaries/Linux/ShooterGameServer ${SERVER_MAP
 ?RCONPort=${RCON_PORT}\
 ?GameModIds=${GAME_MOD_IDS}\
 ?ActiveMods=${GAME_MOD_IDS}\
+?EnableExtraStructurePreventionVolumes=False\
+?PVEAllowStructuresAtSupplyDrops=True\
+?AllowCaveBuildingPVE=True\
 ?ShowFloatingDamageText=True\
 ?AllowFlyerCarryPvE=True\
 ?bAllowPlatformSaddleMultiFloors=True\
